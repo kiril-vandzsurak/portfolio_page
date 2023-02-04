6998beaf5582d3fa40b31156a5504c9cc3554164
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-04 23:38:47 +0100

    creating navbar and jumbotron

--- src/components/HomePart/HomePart.jsx
+++ src/components/HomePart/HomePart.jsx
@@ -1,10 +1,25 @@
+import Button from "react-bootstrap/Button";
+
 const HomePart = () => {
   return (
-    <div className="backgroundHomePart">
-      <h1 style={{ fontSize: "50px", paddingTop: "40px" }}>
-        Hi! I'm Kiril Vandzsurak
-      </h1>
-      <p>Nice to meet you</p>
+    <div className="backgroundHomePart ">
+      <div className="centerJumbotronInscription ">
+        <h1
+          className="interFontBold"
+          style={{ fontSize: "50px", paddingTop: "40px" }}
+        >
+          Hi! I'm Kiril Vandzsurak
+        </h1>
+        <p style={{ fontSize: "20px" }} className="interFontLight">
+          Young and motivated Full-Stack developer
+        </p>
+        <Button style={{ marginTop: "20px" }} size="lg">
+          Here you will find my CV
+        </Button>{" "}
+        <div className="mouse">
+          <div className="mouseDot"></div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/MyNavbar/MyNavbar.jsx
+++ src/components/MyNavbar/MyNavbar.jsx
@@ -3,24 +3,30 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "../../";
 
 const MyNavbar = () => {
   return (
-    <>
-      <Navbar expand="lg">
-        <Container>
+    <div className="sticky">
+      <Navbar style={{ backgroundColor: "rgb(73, 70, 70)" }} expand="lg">
+        <Container className="d-flex justify-content-between">
           <Row>
             <Col>
               <div className="navbarImg"></div>
             </Col>
+          </Row>
+          <Row>
             <Col>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
+              <Navbar.Collapse
+                className="centerNavbarInscription"
+                id="basic-navbar-nav"
+              >
                 <Nav className="me-auto" style={{ marginLeft: "auto" }}>
                   <Nav.Link href="#home">HOME</Nav.Link>
-                  <Nav.Link href="#link">ABOUT ME</Nav.Link>
+                  <Nav.Link href="#link" style={{ width: "94px" }}>
+                    ABOUT ME
+                  </Nav.Link>
                   <Nav.Link href="#link">PROJECTS</Nav.Link>
                 </Nav>
               </Navbar.Collapse>
@@ -28,7 +34,7 @@ const MyNavbar = () => {
           </Row>
         </Container>
       </Navbar>
-    </>
+    </div>
   );
 };
 
