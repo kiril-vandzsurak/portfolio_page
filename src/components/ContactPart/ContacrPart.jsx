68cd2b10b221fd65b63de4492c3eef685d2f637b
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-07 19:16:33 +0100

    minor changes

--- src/components/ContactPart/ContacrPart.jsx
+++ src/components/ContactPart/ContacrPart.jsx
@@ -1,8 +1,37 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 const ContactPart = () => {
   return (
     <>
       <div className="contactPart">
-        <p className="topName">CONTACT ME</p>
+        <img
+          className="backgroundContact"
+          src={window.location.origin + "/contact.jpg"}
+          alt="img"
+        />
+        <p
+          style={{ display: "block", position: "absolute" }}
+          className="topName"
+        >
+          CONTACT ME
+        </p>
+        <div className="contactForm">
+          <Form>
+            <Form.Group className="mb-3 borders" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     </>
   );
